6eabb5440190a475113fbd46379db92488107da8
Tyler Nguyen <ngochoangnguyen@ufl.edu>
2025-09-20 15:57:12 -0400

    adding files

---
+++ backend/controllers/loginAndRegister.js
@@ -0,0 +1,75 @@
+import User from "../models/Users.js";
+import bcrypt from "bcryptjs";
+import cors from "cors";
+
+export const registerUser = async(req, res) => {
+    try {
+        const { username, email, password, role } = req.body;
+    
+        if (!username || !email || !password || !role) {
+          return res.status(400).json({ message: "All fields are required" });
+        }
+    
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ message: "Email already registered" });
+        }
+    
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        const newUser = new User({
+          username,
+          email,
+          password: hashedPassword,
+          role,
+        });
+        
+    await newUser.save();
+
+    const token = jwt.sign(
+        { id: newUser._id, role: newUser.role },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d'}
+    );
+
+    res.status(201).json({
+        message: "User registered successfully",
+        token,
+        user: { id: newUser._id, username: newUser.username, role: newUser.role },
+      });
+    } 
+    
+    catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      if (!email || !password) {
+        return res.status(400).json({ message: "Email and password required" });
+      }
+  
+      const user = await User.findOne({ email });
+      if (!user) return res.status(400).json({ message: "Invalid credentials" });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+  
+      const token = jwt.sign(
+        { id: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
+      );
+  
+      res.json({
+        message: "Login successful",
+        token,
+        user: { id: user._id, username: user.username, role: user.role },
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  };
