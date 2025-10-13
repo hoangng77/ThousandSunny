dc94f44842071bc02a8aa8f5249595241f5a3d42
Daphne <54087715+daphnecalin@users.noreply.github.com>
2025-10-13 17:51:24 -0400

    set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Register from './register.jsx'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Register></Register>
+    </>
+  )
+}
+
+export default App
